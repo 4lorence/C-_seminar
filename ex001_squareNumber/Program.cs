14bfecca75d530384ca5f10c47f2b3a664d66f6b
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2022-11-30 00:04:36 +0500

    add 2 ex

---
+++ ex001_squareNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача №1. Напишите программу, которая на вход принимает два 
+//числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+Console.WriteLine("Enter number1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 * number2 == number1)
+{
+    Console.WriteLine("Wow");
+}
+
+else
+{
+    Console.WriteLine("Oops");
+}
---
+++ ex002_Day/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название 
+//дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine("enter num:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x == 1)
+{
+    Console.WriteLine("Monday");
+}
+
+if(x == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+
+if(x == 3)
+{
+    Console.WriteLine("Wednesnday");
+}
+
+if(x == 4)
+{
+    Console.WriteLine("Thursday");
+}
+
+if(x == 5)
+{
+    Console.WriteLine("Sunday");
+}
+
+if(x == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if(x>7 || x < 1)
+{
+    Console.WriteLine("oops");
+}
