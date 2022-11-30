a03403fe050ef6536f9de169013036fa7fde6af4
4lorence <nikolay.evdo@gmail.com>
2022-11-30 10:20:49 +0500

    add ex004_3num

---
+++ ex004_3num/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход
+//три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("enter number1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter number1:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter number1:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int mx = 
+
+if(num2 > num1)
+{
+    int mx = num2;
+}
+ else if(num3 > mx)
+{
+    int mx = num3;
+}
+Console.WriteLine("max number="+ mx);
