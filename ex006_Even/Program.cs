a4de5372f6f8c76dfef33d0e33b3603c8fcb0dde
4lorence <nikolay.evdo@gmail.com>
2022-11-30 11:48:28 +0500

    add ex006_Even

---
+++ ex006_Even/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+Console.Write("enter any number:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
