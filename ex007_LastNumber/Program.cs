e8a7a5689f844cd59603efe8a6f8d62de6bd3747
4lorence <nikolay.evdo@gmail.com>
2022-11-30 12:07:45 +0500

    add ex007_LastNumber

---
+++ ex007_LastNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Enter a three-digit number:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int LastN = N % 10;
+
+Console.WriteLine(LastN);
