3eeff7c94dc4dcaa06804055647e6fcf7f4c6953
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2022-12-02 22:53:10 +0500

    add ex011_DelSecondN

---
+++ ex011_DelSecondN/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int N = new Random().Next(100,1000);
+Console.WriteLine(N);
+
+int firstN = N /100;
+int lastN  = N % 10;
+int res = firstN * 10 + lastN;
+
+Console.WriteLine(res);
