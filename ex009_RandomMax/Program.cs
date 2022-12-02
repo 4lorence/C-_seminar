859e1524082f125d83caaab4e4f240a71627488e
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2022-12-02 22:27:13 +0500

    add ex009_RandomMax

---
+++ ex009_RandomMax/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int N = new Random().Next(10,100);
+int LastN = N % 10;
+int N10 = N / 10;
+// Console.WriteLine("Random number=" + N "first number=" + N10 "second number=" + LastN);
+Console.WriteLine($"random number ={N}, first number = {N10}, second number = {LastN}");
+int max = N10;
+if(max < LastN)
+{
+    max = LastN;
+}
+Console.WriteLine(max);
