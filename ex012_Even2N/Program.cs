4fdcaf409874ed3c0f9b3757f9b830654f0c6135
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2022-12-02 23:18:53 +0500

    add ex012 and ex014

---
+++ ex012_Even2N/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("enter two numbers");
+int firstN = Convert.ToInt32(Console.ReadLine());
+int SecN = Convert.ToInt32(Console.ReadLine());
+
+if(firstN % SecN == 0)
+{
+    Console.WriteLine("multiple");
+}
+else
+{
+    int N = firstN % SecN;
+    Console.WriteLine($"repeatedly, остаток{N}");
+}
