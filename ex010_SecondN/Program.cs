d121b518e93fabd8a6e107038561d9c3618a297a
4lorence <nikolay.evdo@gmail.com>
2022-12-05 09:27:10 +0500

    add ex010_SecondN

---
+++ ex010_SecondN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Enter a three-digit number:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 1000)
+{
+    Console.WriteLine("it's a three digit number");
+}
+
+else
+{
+int tempN = N / 10;
+int res = tempN % 10;
+Console.WriteLine(res);
+}
