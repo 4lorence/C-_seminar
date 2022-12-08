4cda5343cf3f396249a4f5abddf2d4ea2dcb68fb
4lorence <nikolay.evdo@gmail.com>
2022-12-08 14:09:22 +0500

    add 023_Cube

---
+++ ex023_Cube/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Enter a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Values of cube numbers from 1 to {N}: ");
+for (int i=1; i<=N; i++)
+{
+    Console.Write(Math.Pow(i,3)+" ");
+}
