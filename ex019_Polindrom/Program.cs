3d8cd77c56317d1a9c4364e17ecc777885acebc8
4lorence <nikolay.evdo@gmail.com>
2022-12-09 09:18:35 +0500

    add correction ex019_Polindom

--- ex019_Polindrom/Program.cs
+++ ex019_Polindrom/Program.cs
@@ -1,32 +1,27 @@
 ﻿// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.Write("enter a five-digit number: ");
+Console.Write("enter a five-digit N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-int div = N /10000;
-int rem = N % 10;
+int num1 = N / 10000 % 10;
+int num2 = N / 1000 % 10;
+int rev1 = N / 10 % 10;
+int rev2 = N % 10;
+int nul = N / 10000;
+        
+if (nul < 1 || nul > 9){
+    Console.WriteLine($"{N}Not 5-digit N");
+    }
+    else if  (num1 == rev2 && num2 == rev1)
+    {
+        Console.WriteLine($"Yes, this is {N} palindrom");
+    }
+    else
+    {
+        Console.WriteLine($"No, this is {N}  not palindrom");
+    }
 
-if(N >= 10000)
-{
-    div = N / 10000; 
-    rem = N % 10; 
-    if (div == rem)
-     {
-            N = N /10;
-            int div1 = (N / 100) % 10;
-            int rem2 = N % 10;
-                Console.WriteLine($"yeah, number {N} palindrome");
-     
-     }
-     else
-        {
-            Console.WriteLine($"no, number {N}  not a palindrome");
-        }  
-}
-else
-{
-    Console.WriteLine("incorrect number");
-}
+
 
 
