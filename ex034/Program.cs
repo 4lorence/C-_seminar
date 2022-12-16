ed4a1c3429b8e76e7a6dc760c36b98a30783b824
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2022-12-16 11:32:14 +0500

    add ex034

---
+++ ex034/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array= new int[123];
+
+for(int i=0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(1,151);
+}    
+int countEven=0;
+for(int j=0;j<array.Length; j++)
+{
+    if(array[j]%2==0)
+    {
+        countEven++;
+    }
+}
+
+Console.WriteLine($"{string.Join(", ", array)}");
+Console.WriteLine($" -> In this array are {countEven} even elements.");
