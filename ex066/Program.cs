4ca5cd84998dc43520bd8a3605b95bb4a78dc2a4
Evdokimov Nikolay <nikolay.evdo@gmail.com>
2023-01-01 14:10:43 +0500

    rename ex06

---
+++ ex066/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int start = ReadInt("Enter N: ");
+int end = ReadInt("Enter M: ");
+if (end<start)
+{
+    int temp = start;
+    start = end;
+    end = temp;
+}
+Console.Write($"N = {start}; M = {end} -> ");
+Console.Write(SumNaturalNumbers(start,end));
+
+int SumNaturalNumbers(int n, int m)
+{
+    if (m==n)        
+        return m;
+    return m + SumNaturalNumbers(n,m-1);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
